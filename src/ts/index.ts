65459e645f298b3970457140dc9d004be17227db
gena <genagreiner94@gmail.com>
2023-02-05 16:43:37 +0200

    feat: removed redundant files

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,13 +1,2 @@
 import '../assets/scss/style.scss';
-import './template';
-import router from './router';
-import template from './template';
-
-router();
-
-export default function functions() {
-  template();
-  
-  // here you should put any functions as you need!
-  
-}
+import '../pages/index.html';
