1b6520928ceba924284ed27bd529880cb38ded2c
Svetlanahrhr <maadlana@mail.ru>
2023-02-07 16:31:28 +0400

    feat: start draw phone

--- src/ts/router/renderNewPage.ts
+++ src/ts/router/renderNewPage.ts
@@ -1,12 +1,17 @@
 import { drawMainPage } from '../draw-page/draw-main-page';
+import { drawWebsiteURLPage } from '../draw-page/drawWebsiteURLPage';
 import { breadcrumbsHighlight } from '../state-element';
 
 export default function renderNewPage(idPage: string) {
     const container = document.querySelector('main') as HTMLElement;
-    container.innerHTML = '';
-
     if (idPage === 'main') {
+        container.innerHTML = '';
         container.append(drawMainPage());
+        breadcrumbsHighlight();
+    }
+    if (idPage === 'WebsiteURL') {
+        container.innerHTML = '';
+        container.append(drawWebsiteURLPage());
         breadcrumbsHighlight();
     }
 }
--- src/ts/router/routChange.ts
+++ src/ts/router/routChange.ts
@@ -3,8 +3,7 @@ import renderNewPage from './renderNewPage';
 export default function routeChange() {
     window.addEventListener('hashchange', () => {
         const hash = window.location.hash.slice(1);
-        const id = window.location.hash.slice(10);
-        console.log({ id });
+        console.log({ hash });
 
         renderNewPage(hash);
     });
