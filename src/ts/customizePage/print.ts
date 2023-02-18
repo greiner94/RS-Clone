28011c513bfcb92a6287af1afb0db2d6cd0e138e
Gennadiy Greiner <98348568+greiner94@users.noreply.github.com>
2023-02-18 19:37:52 +0200

    Fix/small bugs (#15)
    * fix: sms validation form
    
    * fix: bug with incorrect save
    
    * fix: form validation and view of calendar allDay button
    
    * fix: correct save, download and print when smartphone preview hide
    
    * fix: bug when use final buttons and smatphone preview hidden

--- src/ts/customizePage/print.ts
+++ src/ts/customizePage/print.ts
@@ -3,6 +3,9 @@ import domtoimage from 'dom-to-image';
 export default function print() {
     const printButton = document.querySelector('.btn-print') as HTMLElement;
     printButton.addEventListener('click', () => {
+        const smatphoneDisplayState =
+            (document.querySelector('.smartphone') as HTMLElement).style.display == 'block' ? 'block' : '';
+        (document.querySelector('.smartphone') as HTMLElement).style.display = 'block';
         const qrElement = document.querySelector('.template__preview-qr-wrapper') as HTMLElement;
         domtoimage.toPng(qrElement).then((dataUrl) => {
             const a = window.open('', '', 'height=500, width=500');
@@ -12,6 +15,7 @@ export default function print() {
                 a.document.write('</body></html>');
                 a.document.close();
                 a.print();
+                (document.querySelector('.smartphone') as HTMLElement).style.display = smatphoneDisplayState;
             }
         });
     });
--- src/ts/customizePage/save.ts
+++ src/ts/customizePage/save.ts
@@ -7,9 +7,15 @@ export default function save() {
     saveBtn.addEventListener('click', () => {
         if (saveBtn.getAttribute('disabled')) return;
         saveBtn.setAttribute('disabled', 'true');
+        const smatphoneDisplayState =
+            (document.querySelector('.smartphone') as HTMLElement).style.display == 'block' ? 'block' : '';
+        (document.querySelector('.smartphone') as HTMLElement).style.display = 'block';
+
         const qrElement = document.querySelector('.template__preview-qr-wrapper') as HTMLElement;
         const { type, descr } = JSON.parse(localStorage.getItem('qrBody') || '');
         domtoimage.toPng(qrElement).then((dataUrl) => {
+            (document.querySelector('.smartphone') as HTMLElement).style.display = smatphoneDisplayState;
+
             fetch(`https://qr-api-vks7.onrender.com/api/qr/ready/${getUserID()}`, {
                 method: 'POST',
                 headers: {
